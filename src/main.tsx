a4b6f43ad71fe24eae29066eb601c8d762a1e4d3
viet02012007-code <viet02012007@gmail.com>
2026-03-30 12:02:19 +0700

    Optimize URL with BrowserRouter and GH Pages SPA redirect

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './config/firebase'
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/SmileyCreative">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
